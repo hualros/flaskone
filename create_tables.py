3b36f2c1310dc28a3a5f184408130732b427bcd2
Hugo Alvarado <hualro@gmail.com>
2020-05-24 16:28:51 -0500

    addming id to items table

--- create_tables.py
+++ create_tables.py
@@ -6,11 +6,11 @@ cursor = connection.cursor()
 create_table = "create table if not exists users (id INTEGER PRIMARY KEY, username text, password text)"
 cursor.execute(create_table)
 
-create_table = "create table if not exists items (name text, price real)"
+create_table = "create table if not exists items (id INTEGER PRIMARY KEY, name text, price real)"
 cursor.execute(create_table)
 
-cursor.execute("insert into items values ('apple', 10.99)")
-cursor.execute("insert into items values ('banana', 2.50)")
+cursor.execute("insert into items values (1,'apple', 10.99)")
+cursor.execute("insert into items values (2,'banana', 2.50)")
 
 connection.commit()
 connection.close()
