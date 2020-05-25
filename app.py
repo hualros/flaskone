5ac1ebf5cd2d66bd5ec399d5abafdd3e601cdd4e
Hugo Alvarado <hualro@gmail.com>
2020-05-24 22:23:07 -0500

    adding run for heroku, fixing db access.

--- app.py
+++ app.py
@@ -14,13 +14,6 @@ app.secret_key = 'hugo'
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-# /auth
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(StoreList, '/stores/')
